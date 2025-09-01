00e6673840b75259b3f4bf3f297b36a56d1051c9
1ofmath <1ofmath@gmail.com>
2025-09-01 23:50:25 +0900

    mainETF 수정

--- src/monthETF/mainMonthETF.py
+++ src/monthETF/mainMonthETF.py
@@ -1,14 +1,18 @@
 import subprocess
+import os
 
 def run_scripts():
     try:
+        # 현재 스크립트의 디렉토리 경로
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        
         # 첫 번째 프로그램 실행
         print(">>> getMonthETF.py 실행 중...")
-        subprocess.run(["python", "getMonthETF.py"], check=True)
+        subprocess.run(["python", os.path.join(script_dir, "getMonthETF.py")], check=True)
 
         # 두 번째 프로그램 실행
         print(">>> updateData.py 실행 중...")
-        subprocess.run(["python", "updateData.py"], check=True)
+        subprocess.run(["python", os.path.join(script_dir, "updateData.py")], check=True)
 
         print(">>> 모든 프로그램 실행 완료!")
 
